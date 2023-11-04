ab50da08101a6c1b4c1b949b18378dfcfbf65f57
Ansheng Zhou <azhou@lcs.on.ca>
2023-11-04 11:44:35 -0400

    trying to sub in navigation stack

--- RemindersApp/ContentView.swift
+++ RemindersApp/ContentView.swift
@@ -9,41 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            //            First layer(background)
-
-                        
-            //            Second layer(rest of interface)
+        NavigationStack {
             VStack {
-                Spacer()
-                List {
-                    Group {
-                        Text("To Do")
-                            .font(.system(.largeTitle, design: .rounded))
-                            .fontWeight(.bold)
-                            .foregroundColor(.blue)
-                        ToDoListView(toDoName: "Call auto body shop",
-                                     toDoTime: "2023-11-16")
-                        ToDoListView(toDoName: "Feed Piper",
-                                     toDoTime: "Tomorrow, 8:00 AM, Daily")
-                        ToDoListView(toDoName: "Write lesson about interactive apps",
-                                     toDoTime: "Tomorrow, 6:00 PM")
-                        ToDoListView(toDoName: "Take piper to the vet",
-                                     toDoTime: "2023-11-29, 3:00 PM")
-                        ToDoListView(toDoName: "Drop off donation at Goodwill",
-                                     toDoTime: "2023-11-11")
-                        ToDoListView(toDoName: "Call plumber about clog in sink",
-                                     toDoTime: "Tomorrow, 2:00 PM")
-                        ToDoListView(toDoName: "Call dentist to schedule appointment",
-                                     toDoTime: "2023-11-06, 4:00 PM")
-                        ToDoListView(toDoName: "Call mechanic to get TARDIS repaired",
-                                     toDoTime: "Tomorrow, 12:00 PM")
+                ZStack {
+                    //            First layer(background)
+
+                                
+                    //            Second layer(rest of interface)
+                    VStack {
+                        Spacer()
+                        List {
+                            Text("To Do")
+                            Group {
+                                ToDoListView(toDoName: "Call auto body shop",
+                                             toDoTime: "2023-11-16")
+                                ToDoListView(toDoName: "Feed Piper",
+                                             toDoTime: "Tomorrow, 8:00 AM, Daily")
+                                ToDoListView(toDoName: "Write lesson about interactive apps",
+                                             toDoTime: "Tomorrow, 6:00 PM")
+                                ToDoListView(toDoName: "Take piper to the vet",
+                                             toDoTime: "2023-11-29, 3:00 PM")
+                                ToDoListView(toDoName: "Drop off donation at Goodwill",
+                                             toDoTime: "2023-11-11")
+                                ToDoListView(toDoName: "Call plumber about clog in sink",
+                                             toDoTime: "Tomorrow, 2:00 PM")
+                                ToDoListView(toDoName: "Call dentist to schedule appointment",
+                                             toDoTime: "2023-11-06, 4:00 PM")
+                                ToDoListView(toDoName: "Call mechanic to get TARDIS repaired",
+                                             toDoTime: "Tomorrow, 12:00 PM")
+                            }
+                        }.listStyle(.grouped)
+                        
                     }
-                }.listStyle(.grouped)
+                    
+                }
+                .toolbar {
+                    ToolbarItem(placement:.topBarLeading) {
+                        Text("Lists")
+                    }
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Image(systemName:"ellipsis.circle")
+                            .padding(.horizontal)
+                    }
+                }
+                .navigationBarTitleDisplayMode(.inline)
                 
             }
-            
         }
+       
 //        .padding()
     }
 }
