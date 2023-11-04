06890117fca287eec76b658a928dcf7e379fed28
Ansheng Zhou <azhou@lcs.on.ca>
2023-11-04 09:48:45 -0400

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Ansheng Zhou on 2023-11-04.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
